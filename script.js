359a47e67a57b1b1e0e9d61453e834e7288c1a64
jonabolli <jonabohlinger2@gmail.com>
2025-12-15 12:15:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+js
 
+var tree = new FamilyTree (document.getElementById("tree"), {
+  nodes: personen,
+  nodeBinding: {
+    field_0: "name",
+    field_1: "geb"
+  },
+  enableSearch: true, 
+  zoom: true,
+  nodeMouseClick: FamilyTree.action.details
+});
