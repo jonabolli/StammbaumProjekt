681bdc5d9eb60a6c1f8d96914ff1bdc384b162ea
jonabolli <jonabohlinger2@gmail.com>
2025-12-15 12:11:01 +0100

    Add initial data for personen array

---
+++ data.js
@@ -0,0 +1,8 @@
+js
+
+var personen = [
+  { id: 1, name: "Abraham", geb: "v.Chr.", info: "Beruf: Hirte" },
+  { id: 2, name: "Sara", geb: "v.Chr.", info: "Mutter" },
+  { id: 3, pid: 1, mid: 2, name: "Ismael", geb: "v.Chr.", info: "Sohn" }
+];
+   
